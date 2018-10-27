a0b5f2970c9843fea717a0d49f628c6ad02c6726
Rob Flynn <rob@thingerly.com>
2018-10-27 03:31:03 -0400

    show window

--- noticekeeper/AppDelegate.swift
+++ noticekeeper/AppDelegate.swift
@@ -18,6 +18,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
         window = UIWindow(frame: UIScreen.main.bounds)
 
+        let mainViewController = MainViewController()
+        mainViewController.view.backgroundColor = .purple
+
+        window?.rootViewController = mainViewController
+
+        window?.makeKeyAndVisible()
+
         return true
     }
 }
---
+++ noticekeeper/MainViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MainViewController.swift
+//  noticekeeper
+//
+//  Created by Rob Flynn on 10/27/18.
+//  Copyright © 2018 Thingerly. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        print("the view loaded")
+    }
+}
